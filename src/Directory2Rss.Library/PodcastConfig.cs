8e725b335f2dee610eec787b37ab85487bb4527a
Adam Carter <mail@adam-carter.com>
2020-08-16 20:54:24 -0700

    fixed broken links to old project name.

---
+++ src/Directory2Rss.Library/PodcastConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Directory2Rss.Library
+{
+    public class PodcastConfig
+    {
+        public string DirectoryToServe { get; set; }
+        public List<string> AudioExtensions { get; set; }
+        public string IPAddress { get; set; }
+        public string PodcastTitle { get; set; }
+        public string PodcastDescription { get; set; }
+        public string PodcastOwner { get; set; }
+        public string PodcastCategory { get; set; }
+
+        public PodcastConfig()
+        {
+            DirectoryToServe = Directory.GetCurrentDirectory();
+            IPAddress = GetLocalIPAddress();
+            AudioExtensions = new List<string>();
+            AudioExtensions.Add(".mp3");
+            PodcastTitle = "Directory2Rss";
+            PodcastDescription = "A directory being served as a podcast RSS feed";
+            PodcastOwner = "Directory2Rss";
+            PodcastCategory = "Music";
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+    }
+}
---
+++ src/Directory2Rss.Library/PodcastRss.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace Directory2Rss.Library
+{
+    public class PodcastRss
+    {
+        private List<PodcastItem> _items = new List<PodcastItem>();
+
+        public PodcastConfig Config { get; set; }
+
+        public PodcastRss(PodcastConfig config)
+        {
+            Config = config;
+        }
+
+        public void AddItem(PodcastItem item)
+        {
+            _items.Add(item);
+        }
+
+        public string ToXml()
+        {
+            var itemsXml = new StringBuilder();
+            foreach(var item in _items)
+            {
+                itemsXml.Append(item.ToXml());
+            }
+
+            string xml = string.Format(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<rss xmlns:itunes=\"http://www.itunes.com/dtds/podcast-1.0.dtd\" version=\"2.0\">\n" +
+                    "<channel>\n" +
+                        "<title>{0}</title>\n" +
+                        "<link>{1}</link>\n" +
+                        "<language>en-us</language>\n" +
+                        "<itunes:author>{2}</itunes:author>\n" +
+                        "<description>{3}</description>\n" +
+                        "<itunes:owner>\n" +
+                            "<itunes:name>{4}</itunes:name>\n" +
+                        "</itunes:owner>\n" +
+                        "<itunes:explicit>no</itunes:explicit>\n" +
+                        "<itunes:image href=\"{5}\" />\n" +
+                        "<itunes:category text=\"{6}\"/>\n" +
+                        "{7}\n" +
+                    "</channel>\n" +
+                "</rss>",
+                Config.PodcastTitle,
+                string.Format("http://{0}", Config.IPAddress),
+                Config.PodcastOwner,
+                Config.PodcastDescription,
+                Config.PodcastOwner,
+                string.Format("http://{0}/image", Config.IPAddress),
+                Config.PodcastCategory,
+                itemsXml
+                );
+            return xml;
+        }
+    }
+}
